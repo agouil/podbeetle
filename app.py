024e53ff15d7b6c3d3a5e5db5fc85664e6605bba
Andreas Williams <andreas.williams12@gmail.com>
2016-09-27 16:03:57 +0100

    add database module

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from database import db_session
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
