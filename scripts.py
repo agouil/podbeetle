a38b19df7221011a1438c89b7e39a190f3cb40d1
Andreas Williams <andreas.williams12@gmail.com>
2016-10-03 15:29:24 +0100

    add 'dump_db' and 'upload_deepgram' scripts

--- scripts.py
+++ scripts.py
@@ -2,8 +2,11 @@ import argparse
 import csv
 import sys
 
+from config import DEEPGRAM_API_KEY
 from database import db_session, init_db
+from deepgram import Deepgram
 from models import Podcast
+from time import sleep
 
 
 def populate_db():
@@ -28,12 +31,66 @@ def populate_db():
         db_session.remove()
 
 
+def dump_db():
+    """
+    Dumps database data into a CSV file.
+    """
+
+    podcasts = Podcast.query.all()
+
+    headers = ["name", "author", "title", "image", "url", "remote_id"]
+    with open("./db/podcast_data_dump.csv", "w") as csvfile:
+        writer = csv.DictWriter(
+            csvfile, fieldnames=headers, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+        for podcast in podcasts:
+            writer.writerow({
+                "name": podcast.name,
+                "author": podcast.author,
+                "title": podcast.title,
+                "image": podcast.image,
+                "url": podcast.url,
+                "remote_id": podcast.remote_id
+            })
+
+
 def deepgram_upload():
     """
     Uploads any new podcasts to Deepgram API
     """
 
-    pass
+    dg = Deepgram(DEEPGRAM_API_KEY)
+
+    podcasts_to_upload = Podcast.query.filter(
+        Podcast.remote_id == "").limit(15).all()
+
+    # podcasts_to_upload = Podcast.query.filter(Podcast.remote_id == "").all()
+
+    for podcast in podcasts_to_upload:
+        upload_response = dg.upload(podcast.url, ["podcast"])
+
+        # check status
+        while True:
+            status_response = dg.check_status(upload_response["contentID"])
+            if status_response["status"] == "fetch":
+                # sleep 1 sec
+                sleep(1)
+                continue
+            elif status_response["status"] in [
+                    "transcode", "chunk", "awaiting_gen_lattice"]:
+                # sleep 5 secs
+                # TODO: periodically check the server until is done
+                break
+            elif status_response["status"] == "done":
+                break
+
+        # update podcast with new remote_id
+        podcast.remote_id = upload_response["contentID"]
+        db_session.add(podcast)
+    # commit changes to db
+    db_session.commit()
+    # close db session
+    db_session.remove()
 
 
 if __name__ == "__main__":
@@ -50,3 +107,5 @@ if __name__ == "__main__":
         deepgram_upload()
     elif args.script == "init_db":
         init_db()
+    elif args.script == "dump_db":
+        dump_db()
